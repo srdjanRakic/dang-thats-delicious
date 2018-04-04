3daf7daeb32570b713c97aaab5590eb238f89a8e
Srdjan Rakic <srgjan.rakic@gmail.com>
2018-04-04 08:42:45 +0200

    Add StoreController

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,4 @@ const storeController = require('../controllers/storeController');
 
 router.get('/', storeController.homePage);
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join("");
-  res.send(reverse)
-});
-
-
 module.exports = router;
